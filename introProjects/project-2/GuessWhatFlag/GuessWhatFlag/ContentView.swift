00238d33644828d4fd0621456769e36b7bc04eeb
Jessica Lewinter <jessicalewinter@gmail.com>
2020-01-14 19:31:37 -0300

    Added project-2

---
+++ introProjects/project-2/GuessWhatFlag/GuessWhatFlag/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  GuessWhatFlag
+//
+//  Created by Jessica Lewinter on 14/01/20.
+//  Copyright © 2020 Jessica Lewinter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+    @State var showingScore = false
+    @State var scoreTitle = ""
+    @State var userPoints = 0
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct!"
+            userPoints += 1
+        } else {
+            scoreTitle = "Wrong!"
+        }
+        showingScore = true
+    }
+    
+    func askAnswer() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    var body: some View {
+        ZStack {
+//            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of:")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                
+                Spacer()
+            }.alert(isPresented: self.$showingScore) {
+                Alert(title: Text(self.scoreTitle), message: Text("Your score is \(self.userPoints)"), dismissButton: .default(Text("OK!"), action: {
+                    self.askAnswer()
+                }))
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
