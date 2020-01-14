5c75f6e22bac68d36a97e92ce251eeaabb9e8263
Jessica Lewinter <jessicalewinter@gmail.com>
2020-01-14 17:13:41 -0300

    Adding project-1

---
+++ introProjects/project-1/SplitBills/SplitBills/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  SplitBills
+//
+//  Created by Jessica Lewinter on 14/01/20.
+//  Copyright © 2020 Jessica Lewinter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let totalAmount = Double(checkAmount) ?? 0
+        let people = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let perPerson = totalAmount/people
+        let withTip = perPerson * 0.01 * tipSelection + perPerson
+        return withTip
+    }
+    
+    var totalAmount: Double {
+        let totalAmount = Double(checkAmount) ?? 0
+        let tipsSelection = Double(tipPercentages[tipPercentage])
+        let withTip = totalAmount * (1 + tipsSelection * 0.01)
+        return withTip
+    }
+    
+        
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Amount Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Total Amount")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("Split Bills")
+        }
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
